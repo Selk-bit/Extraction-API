ad8510bebc10f04fd77504e7e4aaf94a63dcf529
unknown <selkellouti233@gmail.com>
2025-01-03 21:41:33 +0100

    install portable libreoffice

--- libreOfice.sh
+++ libreOfice.sh
@@ -15,9 +15,9 @@ wget https://download.documentfoundation.org/libreoffice/stable/24.2.7/deb/x86_6
 tar -xzf LibreOffice_24.2.7_Linux_x86-64_deb.tar.gz
 
 # Install the DEB packages
-dpkg -i LibreOffice_24.2.7_Linux_x86-64_deb/DEBS/*.deb
+dpkg -i LibreOffice_24.2.7.2_Linux_x86-64_deb/DEBS/*.deb
 
 # Cleanup
-rm -rf LibreOffice_24.2.7_Linux_x86-64_deb.tar.gz LibreOffice_24.2.7_Linux_x86-64_deb
+rm -rf LibreOffice_24.2.7_Linux_x86-64_deb.tar.gz LibreOffice_24.2.7.2_Linux_x86-64_deb
 
 cd $HOME/project/src 
