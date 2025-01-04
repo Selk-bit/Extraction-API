a66e1ffaed3fbcb258e70399dc8e72e8be795580
unknown <selkellouti233@gmail.com>
2025-01-04 23:21:49 +0100

    word ocr

--- libreOfice.sh
+++ libreOfice.sh
@@ -15,7 +15,8 @@ wget https://download.documentfoundation.org/libreoffice/stable/24.2.7/deb/x86_6
 tar -xzf LibreOffice_24.2.7_Linux_x86-64_deb.tar.gz
 
 # Install the DEB packages
-dpkg -i LibreOffice_24.2.7.2_Linux_x86-64_deb/DEBS/*.deb
+# dpkg -i LibreOffice_24.2.7.2_Linux_x86-64_deb/DEBS/*.deb
+dpkg -x LibreOffice_24.2.7.2_Linux_x86-64_deb/DEBS/*.deb $STORAGE_DIR/libreoffice
 
 # Cleanup
 rm -rf LibreOffice_24.2.7_Linux_x86-64_deb.tar.gz LibreOffice_24.2.7.2_Linux_x86-64_deb
