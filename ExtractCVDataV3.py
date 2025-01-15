a038fa0c1fb24a5470ed4833bd2813488e7e18e1
unknown <selkellouti233@gmail.com>
2025-01-15 21:12:28 +0100

    remove extra data

--- ExtractCVDataV3.py
+++ ExtractCVDataV3.py
@@ -955,6 +955,7 @@ class ExtractCVInfos:
                                     section_text = re.sub(r'', '', section_text)
                                     section_text = section_text.replace("\\n", "").replace("\n", "")
                                     section_text = self.replace_unbalanced_quote(section_text)
+                                    section_text = re.sub(r"}].*", "}]", section_text)
                                     temp_work = json.loads(section_text, strict=False)
                                     for key_work, work in enumerate(temp_work):
                                         temp_work[key_work]["responsibilities"] = self.convert_to_html(work["responsibilities"]) if self.convert_to_html(work["responsibilities"]).replace("\n", "") else ""
